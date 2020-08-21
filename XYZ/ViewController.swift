c19e29200d350a3acf5e9706149bd3d51809126a
Asset Ryskul <asset89@gmail.com>
2020-08-21 14:23:17 +0600

    Add CoreMotion

--- XYZ/ViewController.swift
+++ XYZ/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var motionManager: CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels(data:error:))
+        
+    }
+    
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else { return }
+        
     }
 
 
